48d7b112a73a9f0e0f78f5569c69973dda515115
Earnw1ng <earnwing@gmail.com>
2013-10-23 11:08:52 +0200

    renamed tools->dev

--- tools/VocabTrainerStandalone/voca-trainer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 
- * @author rellek
- * @note insert interactive shell here
- */
-
-require_once 'src/VocabFileParser.php';
-$parser = new VocabFileParser();
-$parser->addVocabByFiles("test.txt");
